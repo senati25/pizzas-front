67fab2b5056963dd82d9e1bef7912c6af3df95c7
Philip Perez Castro <jperez.c92@gmail.com>
2021-04-09 22:51:01 -0500

    fix bad import

--- app/components/Dashboard/SubCategories/NewSubcategorieForm/index.jsx
+++ app/components/Dashboard/SubCategories/NewSubcategorieForm/index.jsx
@@ -1,21 +1,45 @@
-import React, { useState } from 'react'
-import useCategories from '../../../../hooks/useCategories';
-import useSubCategories from '../../../../hooks/useSubCategories'
-import DashboardForm from '../../../shared/DashBoardForm'
-import Categories from '../../Categories';
+import React from 'react';
+import useSubCategories from '../../../../hooks/useSubCategories';
+import DashboardForm from '../../../shared/DashBoardForm';
 
 export const FormCreateSubCategorie = () => {
-    const {createSubCategory,responseCreate,handleChange,handleSubmit,handleShowNewCategoryForm}=useSubCategories();
-    
-    return (
-        <DashboardForm handleSubmit={handleSubmit}
-        title="Crear nueva Subcategoria"
-        onCancel={()=>{alert("hola mundo en react")}} >
-           <input type="text" name="denominacion" id="denominacion" placeholder="DENOMINACION" onChange={handleChange}/>
-           <input type="number" name="precio"  id="precio" placeholder="PRECIO" min="0" onChange={handleChange}/>
-           <input type="number" name="categoria_id"   placeholder="CATEGORIA ID" onChange={handleChange}/>
-           
-        </DashboardForm>
+  const {
+    createSubCategory,
+    responseCreate,
+    handleChange,
+    handleSubmit,
+    handleShowNewCategoryForm,
+  } = useSubCategories();
 
-    )
-}
+  return (
+    <DashboardForm
+      handleSubmit={handleSubmit}
+      title="Crear nueva Subcategoria"
+      onCancel={() => {
+        alert('hola mundo en react');
+      }}
+    >
+      <input
+        type="text"
+        name="denominacion"
+        id="denominacion"
+        placeholder="DENOMINACION"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="precio"
+        id="precio"
+        placeholder="PRECIO"
+        min="0"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="categoria_id"
+        placeholder="CATEGORIA ID"
+        onChange={handleChange}
+      />
+    </DashboardForm>
+  );
+};
