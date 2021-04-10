04b2015a787fd229816665488db345715c69135f
Philip Perez Castro <jperez.c92@gmail.com>
2021-04-10 14:32:06 -0500

    edited dashboard nav

--- app/components/DashboardLayout/DashboardNav/index.jsx
+++ app/components/DashboardLayout/DashboardNav/index.jsx
@@ -1,32 +1,47 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import React from 'react';
 import ROUTES from '../../../helpers/constants';
 import styles from './styles.module.css';
 
-const DashboardNav = () => (
-  <nav className={styles.dashboardNav}>
-    <Link
-      href={`/${ROUTES.dashboard.root}/${ROUTES.dashboard.categories.root}`}
-    >
-      <a
-        className={`${styles.dashboardNav__link} ${styles.dashboardNav__link__active}`}
+const DashboardNav = () => {
+  const { pathname } = useRouter();
+  console.log(pathname);
+  return (
+    <nav className={styles.dashboardNav}>
+      <Link
+        href={`/${ROUTES.dashboard.root}/${ROUTES.dashboard.categories.root}`}
       >
-        Categorias
-      </a>
-    </Link>
+        <a
+          className={`${styles.dashboardNav__link} ${
+            pathname.includes('categories') && styles.dashboardNav__link__active
+          } `}
+        >
+          Categorias
+        </a>
+      </Link>
 
-    <Link href="/admin/products">
-      <a className={`${styles.dashboardNav__link} `}>Productos</a>
-    </Link>
+      <Link href="/admin/products">
+        <a
+          className={`${styles.dashboardNav__link} ${
+            pathname.includes('products') && styles.dashboardNav__link__active
+          } `}
+        >
+          Productos
+        </a>
+      </Link>
 
-    <Link href="/admin/clients">
-      <a className={`${styles.dashboardNav__link} `}>Clientes</a>
-    </Link>
-
-    <Link href="/">
-      <a className={`${styles.dashboardNav__link} `}>SubCategorias</a>
-    </Link>
-  </nav>
-);
+      <Link href="/admin/clients">
+        <a
+          className={`${styles.dashboardNav__link} ${
+            pathname.includes('clients') && styles.dashboardNav__link__active
+          } `}
+        >
+          Clientes
+        </a>
+      </Link>
+    </nav>
+  );
+};
 
 export default DashboardNav;
