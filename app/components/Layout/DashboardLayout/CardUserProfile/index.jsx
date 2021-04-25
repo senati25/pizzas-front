4e53e34e7a7ad3e55daf43e8794c1d1c5937f24e
Philip Perez Castro <jperez.c92@gmail.com>
2021-04-25 00:23:50 -0500

    modified CardUserProfile

--- app/components/Layout/DashboardLayout/CardUserProfile/index.jsx
+++ app/components/Layout/DashboardLayout/CardUserProfile/index.jsx
@@ -1,20 +1,25 @@
 import Image from 'next/image';
 import React from 'react';
+import useSessionDashboardContext from '../../../../hooks/useSessionDashboardContext';
 import styles from './styles.module.css';
 
-const CardUserProfile = () => (
-  <div className={styles.cardUserProfile}>
-    <div className={styles.cardUserProfile__img}>
-      <Image
-        src="/images/user-profile.png"
-        width="16"
-        height="16"
-        layout="responsive"
-      />
+const CardUserProfile = () => {
+  const { session } = useSessionDashboardContext();
+
+  return (
+    <div className={styles.cardUserProfile}>
+      <div className={styles.cardUserProfile__img}>
+        <Image
+          src="/images/user-profile.png"
+          width="16"
+          height="16"
+          layout="responsive"
+        />
+      </div>
+      <h3 className={styles.cardUserProfile__name}>{session.nombre}</h3>
+      <p className={styles.cardUserProfile__rol}>Cargo</p>
     </div>
-    <h3 className={styles.cardUserProfile__name}>Nombre</h3>
-    <p className={styles.cardUserProfile__rol}>Cargo</p>
-  </div>
-);
+  );
+};
 
 export default CardUserProfile;
