5cd9840a4af42af78bd3fe3c607a81c26efdc63b
Philip Perez Castro <jperez.c92@gmail.com>
2021-06-12 12:58:37 -0500

    ...

--- app/components/MyAccount/OrderDetails/index.jsx
+++ app/components/MyAccount/OrderDetails/index.jsx
@@ -1,32 +1,42 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import OrderDetailsTable from '../../shared/OrderDetailsTable';
+import { useEffect, useState } from 'react';
+import OrderRepository from '../../../../api/OrderRepository';
+import OrderDetailsContent from '../../shared/OrderDetailsContent';
 import Spinner from '../../shared/Spinner';
 import Container from '../Container';
-import styles from './styles.module.css';
 
 const OrderDetails = () => {
   const router = useRouter();
   const [order, setOrder] = useState(null);
-  useEffect(() => {
-    if (router?.query?.order) {
-      setOrder(JSON.parse(router.query.order));
+  const [isLoading, setIsLoading] = useState({});
+  const [_isMounted, setIsMounted] = useState(true);
+
+  const getDetails = async (orderId) => {
+    setIsLoading(true);
+    const data = await OrderRepository.getById(orderId);
+    if (!data.error && _isMounted) {
+      setOrder(data.payload);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
     }
+  };
+
+  useEffect(() => {
+    if (router?.query?.id) {
+      getDetails(router.query.id);
+    }
+
+    return () => {
+      setIsMounted(false);
+    };
   }, [router.query]);
 
-  if (!order) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
   return (
-    <Container title={`Pedido No. ${order.id}  |  ${order.fecha}`}>
-      <div className={styles.wrapper}>
-        <h2 className={styles.status}>
-          Estado: {order.estado_pedido_denominacion}
-        </h2>
-
-        {order && <div>{order.mensaje}</div>}
-
-        <OrderDetailsTable order={order} />
-      </div>
+    <Container>
+      <OrderDetailsContent order={order} orderStatus={[]} />
     </Container>
   );
 };
